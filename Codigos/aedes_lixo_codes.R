191a41274d725a33ac7b88834dfad0fb6d406dd6
claudioalvesmonteiro <claudiomonteiro@protonmail.com>
2018-03-20 05:59:17 -0300

    commit data .0

---
+++ Codigos/aedes_lixo_codes.R
@@ -0,0 +1,22 @@
+#==============================================================#
+# Observatorio de Saneamento e Meio Ambiente do Recife (OSAR)  #
+# Departamento de Pesquisa                                     #
+#==============================================================#
+# Relacao Entre Coleta de Lixo e Doencas                       #
+# Transmitidas pelo Aedes Aegypti                              #
+#--------------------------------------------------------------#
+# Recife, 2018                                                 #
+#--------------------------------------------------------------#
+
+# carregar pacotes
+
+# carregar dados
+
+
+
+
+
+
+
+
+
